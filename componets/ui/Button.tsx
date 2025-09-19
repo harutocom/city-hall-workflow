6f42f0947e4eff5f05f513a8f9591ee917f28672
Haruto Kobayashi <haru10.com@icloud.com>
2025-09-19 17:22:29 +0900

    Revert "Revert "feat:ログイン画面UI作成""

---
+++ componets/ui/Button.tsx
@@ -0,0 +1,3 @@
+export default function Button({placeholder}: {placeholder: string}) {
+  return <button className="bg-[#CB223F] h-[56px] w-[368px] text-white rounded-[8px]">{placeholder}</button>;
+}
