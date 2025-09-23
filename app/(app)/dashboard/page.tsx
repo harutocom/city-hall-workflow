a331f4e7e1b05ecac9713f184c8179991b7bb3d7
t125083 <t125083@ed.sus.ac.jp>
2025-09-23 21:08:06 +0900

    修正

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,21 +1,3 @@
-import Image from 'next/image';
-import TextEntry from '@/componets/featurses/TextEntry';
-import Select from '@/componets/featurses/Select';
-import TextArea from '@/componets/featurses/TextArea';
-import Radio from '@/componets/featurses/Radio';
-import Check from '@/componets/featurses/Check';
-import Date from '@/componets/featurses/Date';
-import DateRange from '@/componets/featurses/DateRange';
-
 export default function Dashboard() {
-  return <>
-  <div className='flex flex-col gap-[16px]'>
-    <TextEntry></TextEntry>
-    <Select></Select>
-    <Radio></Radio>
-    <Check></Check>
-    <Date></Date>
-    <DateRange></DateRange>
-  </div>
-  </>;
+  return <>Dashboard Page</>;
 }
