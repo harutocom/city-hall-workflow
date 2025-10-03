a9c32c8a48b6e2b08b16887ad4dee26a5ff0370e
yuu-1230 <yuto.nagata1230@gmail.com>
2025-10-03 12:17:46 +0900

    ヘッダー固定

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "./../../componets/header";
+
 export default function MainLayout({
   children,
 }: Readonly<{
@@ -6,7 +7,9 @@ export default function MainLayout({
 }>) {
   return (
     <>
-      <Header />
+      <header className="fixed top-0 left-0 w-full z-50">
+        <Header />
+      </header>
       <main className="pt-16">{children}</main>
     </>
   );
