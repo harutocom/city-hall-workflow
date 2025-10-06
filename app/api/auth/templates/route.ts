357cf4b9ba33cfa296ab1189ac1347e75485015a
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-10-06 09:10:52 +0900

    feat: 認証認可のチェックを追加

--- app/api/auth/templates/route.ts
+++ app/api/auth/templates/route.ts
@@ -1,5 +1,6 @@
 // app/api/templates/route.ts
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 import { db } from "@/lib/db";
 
 // templatesテーブルから一覧を取得する
@@ -35,5 +36,47 @@ export async function GET() {
       { message: "テンプレートデータの取得に失敗しました。" },
       { status: 500 }
     );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    // tokenの情報を取得
+    const token = await getToken({
+      req: request,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+
+    // 念のためtokenが存在するか(ログイン状態かどうか)を確認
+    if (!token) {
+      // tokenが無かったらエラーを返す
+      return NextResponse.json({
+        message: "ログインされていません。",
+        status: 401,
+      });
+    }
+
+    // 取得したtokenから必要な情報を変数へ代入
+    const userId = token.id;
+    const userPermissions = token.permission_ids; // userの権限の配列
+    const targetPermission = 1; // テンプレート作成に必要な権限ID
+
+    //userに権限があるかを確認
+    if (!userPermissions.includes(targetPermission)) {
+      // 権限が無い場合エラーを返す
+      console.error(`status:403 ${userId}はテンプレート作成の権限がありません`);
+      return NextResponse.json({
+        message: "テンプレート作成の権限がありません",
+        status: 403,
+      });
+    }
+
+    const data = await request.json();
+    const a = data;
+
+    return;
+  } catch (error) {
+    console.error(error);
+    return;
   }
 }
